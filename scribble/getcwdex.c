f95d98318e254892a50b6cb231fbe4cb94159565
Christopher Trojan <trojancj@uwec.edu>
2017-04-06 18:42:32 -0500

    Did a bit of work on HW2

--- scribble/getcwdex.c
+++ scribble/getcwdex.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <limits.h>
+
+void parseArgs(char *buffer, char** args, int argsSize, int *nargs);
 
 int main(){
     //pwd stuff
@@ -14,7 +17,32 @@ int main(){
 
     if(cwd != NULL){
         printf("%s>", cwd);
-        printf("%s", fgets(str, 60, stdin));
+        printf("%s\n", fgets(str, 60, stdin));
+        char** args[10];
+        parseArgs(fgets, args, 10); 
     }
     return EXIT_SUCCESS;
+}
+
+void parseArgs(char *buffer, char** args, int argsSize, int *nargs){
+    char *bufArgs[argsSize];
+    char **cp;
+    char *wbuf;
+    int i, j;
+
+    wbuf=buffer;
+    bufArgs[0]=buffer;
+    args[0]=buffer;
+
+    for(cp=bufArgs; (*cp=strsep(&wbuf, "\n\t")) != NULL;){
+        if((*cp != '\0') && (++cp >= &bufArgs[argsSize]))
+            break;
+    }
+    for(j=i=0; bufArgs[i] != NULL; i++){
+        if(strlen(bufArgs[i]) > 0)
+            args[j++]=bufArgs[i];
+    }
+    *nargs = j;
+    args[j] = NULL;
+
 }
