6976064037cf23e451304c25552664fed00ab43d
Christopher <trojancj@uwec.edu>
2017-04-27 18:38:17 -0500

    copying website c files

--- lab08C/uwecSocket.h
+++
@@ -1,5 +0,0 @@
-int setupServerSocket(int portno);          //Like new ServerSocket in Java
-int callServer(char* host, into portno);    //Like new Socket in Java
-int serverSocketAccept(int serverSocket);   //Like ss.accept() in Java
-void writeInt(int x, int socket);           //Write an int over the given socketd
-int readInt(int socket);                    //Read an int from the given socket
